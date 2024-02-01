4ac3ae40b07611ca3f847b7887968cd0d90895e4
Mujo78 <mujo.kalabuzic.20@size.ba>
2024-02-01 14:08:19 +0100

    commit -g

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,13 +1,9 @@
-
-export default function Header(){
-    return(
-        <header className="header">
-        <img 
-            src="./images/troll-face.png" alt="n"
-            className="header--image"
-        />
-        <h2 className="header--title">Meme Generator</h2>
-        <h4 className="header--project">React Course - Project 3</h4>
+export default function Header() {
+  return (
+    <header className="header">
+      <img src="./logo.png" alt="n" className="header--image" />
+      <h2 className="header--title">Meme Generator</h2>
+      <h4 className="header--project">React Project</h4>
     </header>
-    )
+  );
 }
